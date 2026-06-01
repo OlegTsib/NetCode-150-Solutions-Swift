0cb467a0bcf4f6682fdb6317584062e3fe5672ee
OlegTsib <olegtsib@gmail.com>
2026-05-31 21:46:55 -0500

    Create 3Sum.swift

---
+++ 3Sum.swift
@@ -0,0 +1,48 @@
+class Solution {
+    // Notes
+    // "a" will always be negative -a
+    // b + c = -a
+    // c = -(a + b)
+    // "c" will always be unique because a pair of unique [a, b] will result in a unique "c" value
+    func threeSum(_ nums: [Int]) -> [[Int]] {
+        var tripples = [[Int]]()
+        let sorted = nums.sorted()
+        sorted.enumerated().forEach { index, value in
+            guard value <= 0 else { return }
+
+            if index > 0, index + 1 < sorted.count, value == sorted[index - 1] {
+                return
+            }
+
+            let pairs = pairSumSorted(nums: sorted, startIndex: index + 1, target: -value) 
+            tripples += pairs.map { [value] + $0 }
+        }
+
+        return tripples
+    }
+
+    func pairSumSorted(nums: [Int], startIndex: Int, target: Int) -> [[Int]] {
+        var leftIndex = startIndex
+        var rightIndex = nums.count - 1
+        var pairs = [[Int]]()
+
+        while leftIndex < rightIndex {
+            let sum = nums[leftIndex] + nums[rightIndex]
+
+            if sum == target {
+                pairs.append([nums[leftIndex], nums[rightIndex]])
+                leftIndex += 1
+                // To avoid duplicate [b, c] pairs, skip "b"
+                while leftIndex < rightIndex && (nums[leftIndex] == nums[leftIndex - 1]) {
+                    leftIndex += 1
+                }
+            } else if sum < target {
+                leftIndex += 1
+            } else {
+                rightIndex -= 1
+            }
+        }
+
+        return pairs
+    }
+}
