e70dee98462925c96cc71a4a5d6a1a80f95f36a9
OlegTsib <olegtsib@gmail.com>
2026-05-31 21:45:42 -0500

    Create TwoIntergerSum2.swift

---
+++ TwoIntergerSum2.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        var left = 0
+        var right = numbers.count - 1
+
+        while left < right {
+            let leftNumber = numbers[left]
+            let rightNumber = numbers[right]
+            let sum = leftNumber + rightNumber
+
+            if sum < target {
+                left += 1
+            } else if sum > target {
+                right -= 1
+            } else {
+                return [left + 1, right + 1]
+            }
+        }
+
+        return []
+    }
+}
